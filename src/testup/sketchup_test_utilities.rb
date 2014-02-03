16641c1345fd549bdc7f4ba1fead4df49184e02a
Thomas Thomassen <thomas@thomthom.net>
2014-02-03 16:33:55 +0100

    Added TC_Animation tests.

--- src/testup/sketchup_test_utilities.rb
+++ src/testup/sketchup_test_utilities.rb
@@ -5,6 +5,49 @@
 
 module TestUp
 
+  class MenuGuard
+
+    # Return a top level menu wrapped in a menu guard that prevent the menus
+    # from being created multiple times.
+    def self.menu(title)
+      @@menus ||= {}
+      unless @@menus.key?(title)
+        @@menus[title] = self.new( UI.menu(title) )
+      end
+      @@menus[title]
+    end
+
+    def initialize(menu)
+      @menu = menu
+      @guarded_menus = {}
+    end
+
+    def add_item(title, &block)
+      unless @guarded_menus.key?(title)
+        menu = @menu.add_item(title, &block)
+        guarded_menu = self.class.new(menu)
+        @guarded_menus[title] = guarded_menu
+      end
+      @guarded_menus[title]
+    end
+
+    def add_separator
+      # No need for test code to add separators. They would be hard to ensure
+      # no duplicates.
+    end
+
+    def add_submenu(title)
+      unless @guarded_menus.key?(title)
+        menu = @menu.add_submenu(title)
+        guarded_menu = self.class.new(menu)
+        @guarded_menus[title] = guarded_menu
+      end
+      @guarded_menus[title]
+    end
+
+  end # class MenuGuard
+
+
   module SketchUpTestUtilities
 
     def start_with_empty_model
