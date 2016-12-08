3699f576cedabaddfa711a2a033ec77cfbd3191b
Thomas Thomassen <thomas@thomthom.net>
2016-12-08 15:44:04 +0100

    Making debug-sketchup.rb cross platform.

--- tools/debug-sketchup.rb
+++ tools/debug-sketchup.rb
@@ -1,28 +1,32 @@
-# This file accepts two arguments which specify which SketchUp version to load.
-#   1. SketchUp version
-#   2. Bitness (optional)
-# Example: ruby debug-sketchup.rb 16 32
+# This file accepts a argument which specify which SketchUp version to load.
+# It assume default installation location.
+# Example: ruby debug-sketchup.rb 16
 sketchup_version = ARGV[0].to_i
-bitness = ARGV[1].to_i
-
-# TODO: Need to set up Mac variant of this.
-program_files_32 = ENV['ProgramFiles(x86)'] || 'C:/Program Files (x86)'
-program_files_64 = ENV['ProgramW6432'] || 'C:/Program Files'
-
-if bitness == 32
-  program_files = program_files_32
-else
-  program_files = program_files_64
-end
 
 # Debugging only possible in SketchUp 2014 and newer as debugger protocol was
 # introduced for Ruby 2.0.
 version = "20#{sketchup_version}"
 
-sketchup_path = File.join(program_files, 'SketchUp', "SketchUp #{version}")
-sketchup = File.join(sketchup_path, 'SketchUp.exe')
+debug_args = '-rdebug "ide port=7000"'
+
+if RUBY_PLATFORM.include?('darwin')
+  # OS X
+  sketchup_path = "/Applications/SketchUp\\ #{version}"
+  sketchup = File.join(sketchup_path, 'SketchUp.app')
+  sketchup_command = %(open -a #{sketchup} --args #{debug_args})
+else
+  # Windows
+  program_files_32 = ENV['ProgramFiles(x86)'] || 'C:/Program Files (x86)'
+  program_files_64 = ENV['ProgramW6432'] || 'C:/Program Files'
+
+  sketchup_relative = "SketchUp/SketchUp #{version}/SketchUp.exe"
+  sketchup_32 = File.join(program_files_32, sketchup_relative)
+  sketchup_64 = File.join(program_files_64, sketchup_relative)
+
+  sketchup = File.exist?(sketchup_64) ? sketchup_64 : sketchup_32
+  sketchup_command = %("#{sketchup}" #{debug_args})
+end
 
 # We then start SketchUp with the special flag to make it connect to the
 # debugger on the given port.
-command = %{"#{sketchup}" -rdebug "ide port=7000"}
-spawn(command)
+spawn(sketchup_command)
