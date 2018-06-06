9b1172d3b7c69d29c6aa5ae95ee5faf6f87832ce
Thomas Thomassen <thomas@thomthom.net>
2018-06-06 13:19:15 +0200

    Tweak Webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -73,20 +73,15 @@ module.exports = {
   ]
 }
 
+// console.log('NODE_ENV', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
+  // https://webpack.js.org/guides/production/#source-mapping
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
+    // https://webpack.js.org/guides/production/#specify-the-environment
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: true
