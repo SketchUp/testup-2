2afb0f8ff251e1e0a52d7f553a324ccd10fab78c
Thomas Thomassen <thomas@thomthom.net>
2018-07-18 14:49:13 +0200

    Add polyfill for Element.closest. Fixes #153.

--- src/testup/ui/js/lib/polyfills.js
+++ src/testup/ui/js/lib/polyfills.js
@@ -141,3 +141,20 @@ if (!String.prototype.includes) {
     }
   };
 }
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+if (!Element.prototype.matches)
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                                Element.prototype.webkitMatchesSelector;
+
+if (!Element.prototype.closest)
+    Element.prototype.closest = function(s) {
+        var el = this;
+        if (!document.documentElement.contains(el)) return null;
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
