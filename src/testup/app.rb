440f50f07597910b753a0b08ccda0b5b4e2c29df
Thomas Thomassen <thomthom@sketchup.com>
2022-01-12 15:35:37 +0100

    Add %CONFIG_DIR% variable to CI YAML config.

--- src/testup/app.rb
+++ src/testup/app.rb
@@ -21,18 +21,31 @@ module TestUp
       self.ci_run_with_config(options[:ci_config_path]) if options[:ci_config_path]
     end
 
+    # @param [String] test_suite Path to the Test Suite to run.
+    # @param [Hash] config
     def self.ci_run(test_suite, config = {})
-      # puts "ci_run"
-      # p config
       Execution.delay(1.0) do
         API.run_suite_without_gui(test_suite, config)
         self.quit unless config['KeepOpen']
       end
     end
 
+    # @param [String] ci_config_path Path to the configuration file to run.
     def self.ci_run_with_config(ci_config_path)
-      config = YAML.load_file(ci_config_path)
+      config = self.read_config_file(ci_config_path)
       self.ci_run(config['Path'], config)
+    end
+
+    # @param [String] ci_config_path Path to the configuration file to parse.
+    def self.read_config_file(ci_config_path)
+      config = YAML.load_file(ci_config_path)
+      config_dir = File.dirname(ci_config_path)
+      # Expand config variables.
+      ['Path', 'Output'].each { |key|
+        value = config[key]
+        value.gsub!('%CONFIG_DIR%', config_dir)
+      }
+      config
     end
 
     def self.quit
